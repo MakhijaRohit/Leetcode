6f36228743474e43e5444bcebcb21af548b7df37
Rohit Makhija <46621145+MakhijaRohit@users.noreply.github.com>
2026-09-02 05:14:55 +0000

    Sync LeetCode submission - Climbing Stairs (cpp)

---
+++ my-folder/problems/climbing_stairs/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int t1 = 0;
+        int t2 = 1;
+        int next = 0;
+        for (int i = 1; i <= n; ++i) {
+            next = t1 + t2;
+            t1 = t2;
+            t2 = next;
+        }
+        return next;
+    }
+};
